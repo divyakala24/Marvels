7cd63e8eaf05109feec6171faac19dd69752b4dc
divya <divyaakala24@gmail.com>
2022-09-28 21:59:58 +0530

    searchbar  Done

--- marvels-app/src/Components/Main.js
+++ marvels-app/src/Components/Main.js
@@ -7,6 +7,7 @@ export const Main = () => {
 const [path,setPath] =useState('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=2942c1fcec2bd56c30c3a84da3d65a17&hash=41ec9558d3576a8856574c48c38d5420')
 const [item,setItem]=useState();
 const [search,setSearch]=useState("");
+
 useEffect(()=>{
     const charData = async()=>{
     const res = await axios.get(path);
@@ -14,6 +15,10 @@ useEffect(()=>{
 }
 charData()
 },[path])
+
+const searchMarvel=()=>{
+    setPath(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&ts=1&apikey=2942c1fcec2bd56c30c3a84da3d65a17&hash=41ec9558d3576a8856574c48c38d5420`)
+  }
   return (
     <>
     <div className="header">
@@ -23,7 +28,10 @@ charData()
         </div>
         <div className="search-bar">
             <img src="./images/Marvels.png" alt="logo" className='logo'/>
-            <input type="search"  placeholder='Search Here' className='search'/>
+            <input type="search"  placeholder='Search Here'
+                 className='search'
+                 onChange={e=>setSearch(e.target.value)}
+                 onKeyPress={searchMarvel}/>
         </div>
     </div>
     <div className="container">
